507975deaf8264d67fd0ea57a9e4680371c97307
Tharnslair <jsmith33@ittesi.com>
2014-08-07 10:14:52 -0400

    init commit

---
+++ Elom/Elom.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Elom.Web.MvcApplication" Language="C#" %>
---
+++ Elom/Elom.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Elom.Web.Startup))]
+namespace Elom.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
