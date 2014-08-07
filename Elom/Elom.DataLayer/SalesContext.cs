c08c85fafb61580f40a8f8b615b54457040d32aa
Tharnslair <jsmith33@ittesi.com>
2014-08-07 12:37:33 -0400

    done with the SalesOrder model...not sure if db creation worked

---
+++ Elom/Elom.DataLayer/SalesContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elom.Model;
+
+namespace Elom.DataLayer
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext() : base("DefaultConnection")
+        {
+            
+        }
+
+        public IDbSet<SalesOrder> SalesOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new SalesOrderConfiguration());
+        }
+    }
+}
